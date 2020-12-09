015632e26d041a9d3cacfec575d58af0b761f9c3
Fabian Neugart <f.neugart@mytaxi.com>
2020-12-09 15:14:02 +0100

    added example code

--- samples/kotlin-gradle/src/main/kotlin/org/jlleitschuh/gradle/ktlint/sample/gradle/main.kt
+++ samples/kotlin-gradle/src/main/kotlin/org/jlleitschuh/gradle/ktlint/sample/gradle/main.kt
@@ -1,5 +1,40 @@
 package org.jlleitschuh.gradle.ktlint.sample.gradle
 
 fun main(args: Array<String>) {
-    (1..10).map { "Value: $it" }.forEach { println("Gradle $it") }
+    Child(1, 2, 3, 4, 5, 6)
+}
+
+abstract class Parent<T : Related>(
+    private var test: Int,
+    test2: Int,
+    var test3: Int,
+    test4: Int,
+    var test5: Int,
+    test6: Int
+)
+
+interface Parent2
+
+open class Related
+
+class Child constructor(
+    test: Int,
+    test2: Int,
+    test3: Int,
+    test4: Int,
+    test5: Int,
+    test6: Int
+) : Parent<Related>(
+    test,
+    test2,
+    test3,
+    test4,
+    test5,
+    test6
+),
+    Parent2 {
+
+    fun test() {
+        println("test")
+    }
 }
